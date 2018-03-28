95af4389acd00ba2b7928dd1f5691904eb45740f
nassredine <nasreddine.skandrani@gmail.com>
2018-03-28 06:10:12 -0400

    add padding to topmenu

--- src/top-menu/top-menu.tsx
+++ src/top-menu/top-menu.tsx
@@ -2,12 +2,16 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
+const stylePadding = {
+  paddingRight: 20
+};
+
 export class TopMenu extends React.Component {
   render() {
     return (
       <div>
-        <Link to={`/`}>Job</Link>
-        <Link to={`/music`}>Music</Link>
+        <Link to={`/`} style={stylePadding}>Job Page</Link>
+        <Link to={`/music`}>Music Page</Link>
       </div>
     );
   }
